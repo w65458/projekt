70377112e297a00511f0f89a550d2b86a30b7c5e
Me <myglobalemail@example.com>
2022-12-30 20:35:57 +0100

    Apache.commons library added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.tictactoe {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires static lombok;
+    requires org.apache.commons.lang3;
 
     opens com.example.tictactoe to javafx.fxml;
     exports com.example.tictactoe;
